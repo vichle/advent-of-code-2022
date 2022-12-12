5c29847e7929a10eae2bf6c450f5479d004dc5dc
Viktor Ljungström <viktor.ljungstrom@gmail.com>
2022-12-12 22:19:55 +0100

    Add shared LCM/GCD functions (shameful interwebz pasta)

--- shared/math.go
+++ shared/math.go
@@ -16,3 +16,27 @@ func ISign(n int) int {
 	}
 	return -1
 }
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(integers ...int) int {
+	a := integers[0]
+	b := integers[1]
+	integers = integers[2:]
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
