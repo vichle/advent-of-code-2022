566c46c36561be83d4ee7b361c6473807f10e9e5
Viktor Ljungström <viktor.ljungstrom@gmail.com>
2022-12-15 09:02:06 +0100

    Improve template

--- template_main.go
+++ template_main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	solution := 0
-	fmt.Printf("Solution: %v\n", solution)
+	solution1 := 0
+	solution2 := 0
+	fmt.Printf("Solution: %v, %v\n", solution1, solution2)
 }
